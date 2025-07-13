967f9db9d2c23203db39bb725da985fd14bbb249
heitormarin83 <heitor.a.marin@gmail.com>
2025-07-13 10:46:34 -0300

    Create __init__.py

---
+++ src/analyzers/__init__.py
@@ -0,0 +1,9 @@
+"""
+Módulo de analisadores para processamento de notícias
+"""
+
+from .news_analyzer import NewsAnalyzer
+from .report_generator import ReportGenerator
+
+__all__ = ['NewsAnalyzer', 'ReportGenerator']
+
