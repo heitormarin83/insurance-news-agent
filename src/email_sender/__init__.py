491a2d371cf151f6bc4b7655cd86f8e12e636fcc
heitormarin83 <heitor.a.marin@gmail.com>
2025-07-31 07:54:19 -0300

    Update __init__.py

--- src/email_sender/__init__.py
+++ src/email_sender/__init__.py
@@ -1,13 +1,10 @@
 """
-Email Sender Package
-VERSÃO CORRIGIDA - Remove importação do gmail_sender que não existe
+Pacote de envio de e-mails do Insurance News Agent
+Expondo as interfaces públicas: EmailManager e EmailTemplate
 """
 
 from .email_manager import EmailManager
 from .email_template import EmailTemplate
-
-# Remove a importação problemática do gmail_sender
-# from .gmail_sender import GmailSender  # ← REMOVIDO
 
 __all__ = [
     'EmailManager',
